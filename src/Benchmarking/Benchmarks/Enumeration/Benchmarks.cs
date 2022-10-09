36e8ec5b47f0638d0d2f547c45ad7d4c976aba86
César Rodríguez <cesar.rrguez@gmail.com>
2022-10-09 15:44:32 +0200

    Add Enumeration benchmark

---
+++ src/Benchmarking/Benchmarks/Enumeration/Benchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarking.Benchmarks.Enumeration;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private readonly EnumerationService _enumerationService = new();
+    private readonly Color _color = Color.LightGreen;
+    private readonly int _number = 21;
+    private readonly string _colorString = "LightGreen";
+
+    [Benchmark]
+    public string EnumToString() => _enumerationService.EnumToString(_color);
+
+    [Benchmark]
+    public string EnumToStringFast() => _enumerationService.EnumToStringFast(_color);
+
+    [Benchmark]
+    public bool EnumIsDefined() => _enumerationService.EnumIsDefined(_number);
+
+    [Benchmark]
+    public bool EnumIsDefinedFast() => _enumerationService.EnumIsDefinedFast(_number);
+
+    [Benchmark]
+    public (bool, Color) EnumTryParse() => _enumerationService.EnumTryParse(_colorString);
+
+    [Benchmark]
+    public (bool, Color) EnumTryParseFast() => _enumerationService.EnumTryParseFast(_colorString);
+}
--- src/Benchmarking/Program.cs
+++ src/Benchmarking/Program.cs
@@ -2,5 +2,6 @@
 
 BenchmarkRunner.Run<Benchmarking.Benchmarks.ArgumentIncrement.Benchmarks>();
 //BenchmarkRunner.Run<Benchmarking.Benchmarks.ConcatStrings.Benchmarks>();
+//BenchmarkRunner.Run<Benchmarking.Benchmarks.Enumeration.Benchmarks>();
 //BenchmarkRunner.Run<Benchmarking.Benchmarks.JSON.Benchmarks>();
 //BenchmarkRunner.Run<Benchmarking.Benchmarks.LINQ.Benchmarks>();
