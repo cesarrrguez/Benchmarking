4c0fd110127bfeb65ef021b179d22f28b665f3ca
César Rodríguez <cesar.rrguez@gmail.com>
2022-10-29 16:00:09 +0200

    Add async task benchmark

---
+++ src/Benchmarking/Benchmarks/AsyncTask/AsyncTaskService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Benchmarking.Benchmarks.AsyncTask;
+
+public class AsyncTaskService
+{
+    public async Task<string> GetStringWithAsync(Task<string> task) => await task;
+
+    public Task<string> GetStringWithoutAsync(Task<string> task) => task;
+}
---
+++ src/Benchmarking/Benchmarks/AsyncTask/Benchmarks.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Threading.Tasks;
+
+namespace Benchmarking.Benchmarks.AsyncTask;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private readonly AsyncTaskService _asyncTaskService = new();
+    private readonly Task<string> _task = Task.FromResult("César Rodríguez");
+
+    [Benchmark]
+    public async Task<string> GetStringWithAsync() => await _asyncTaskService.GetStringWithAsync(_task);
+
+    [Benchmark]
+    public async Task<string> GetStringWithoutAsync() => await _asyncTaskService.GetStringWithoutAsync(_task);
+}
